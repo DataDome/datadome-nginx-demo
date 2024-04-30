e24649af594c507754795a39be2f2557c607e58c
lauro <111757181+laurodd@users.noreply.github.com>
2024-04-30 13:26:28 -0300

    fix: change dynamic module path (#2)

--- nginx-module.sh
+++ nginx-module.sh
@@ -21,16 +21,13 @@ tar -C ${tmp_dir} -xzf ${tmp_dir}/nginx-${nginx_version}.tar.gz
 curl -sLo ${tmp_dir}/datadome_nginx_module.tar.gz https://package.datadome.co/linux/DataDome-Nginx-latest.tgz
 tar -C ${tmp_dir} -zxf ${tmp_dir}/datadome_nginx_module.tar.gz
 
-# Get the name of the DataDome module directory
-datadome_dir=$(basename $(ls ${tmp_dir}/DataDome-NginxDome-* -d1))
-
 # Get the compilation flags used during the compilation of nginx, and remove any --add-dynamic-module flag we find
 # This is important because when compiling the modules, you have to use the same flags that have been used when compiling nginx
 nginx_flags="$(nginx -V 2>&1 | grep -oP 'configure arguments: \K(.*)' | sed -e 's/--add-dynamic-module=\S*//g')"
 echo $nginx_flags
 
 # Launch the nginx configure script with same flags + the DataDome dynamic module
-cd ${tmp_dir}/nginx-${nginx_version} && eval "./configure --add-dynamic-module=../${datadome_dir} ${nginx_flags}"
+cd ${tmp_dir}/nginx-${nginx_version} && eval "./configure --add-dynamic-module=../ ${nginx_flags}"
 
 # Compile the modules
 make -C ${tmp_dir}/nginx-${nginx_version} -f objs/Makefile modules
